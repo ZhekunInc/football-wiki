5cc6a68ca492beddb9e62ed784bb052f0c26c270
ZhekunInc <golovchak.evgen@gmail.com>
2020-04-30 20:44:33 +0300

    change styles for language

--- src/src/settings.py
+++ src/src/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '=c%putkgs%4@c(#4y!nvef!xbzjvd7q5)##h&bg+y@x0suds#_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['zhekun.pythonanywhere.com']
+ALLOWED_HOSTS = ['zhekun.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
