5366aec850f8d6944d67665c73bf57f857b0de04
Yevhen Golovchak <yev.golovchak@gmail.com>
2020-05-08 18:47:53 +0300

    add social providers

--- src/src/settings.py
+++ src/src/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'allauth.socialaccount.providers.facebook',
+    'allauth.socialaccount.providers.google',
     'django_extensions',
     'django.contrib.sitemaps',
 ]
